0b07d1231b39ad90fdd7bcc0f10ad8baadc55da2
Hifdzi Maulana <hifdzimaulana12@gmail.com>
2022-04-13 07:19:26 +0700

    initialize

---
+++ 1.js
@@ -0,0 +1,9 @@
+// Mana Yang lebih besar?
+
+/*
+*Akan diberikan dua angka, silahkan tentukan angka mana yang lebih besar
+*/
+
+const input = [9, 9.0001]
+const output = input[input[0] > input[1] ? 0 : 1]
+console.log(output)
