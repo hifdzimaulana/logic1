54b927ba4d372ce9f02912db96293513cce7c4f3
Hifdzi Maulana <hifdzimaulana12@gmail.com>
2022-04-13 07:57:06 +0700

    soal nomor 5

---
+++ 5.js
@@ -0,0 +1,9 @@
+// Nilai Maksimum
+
+/**
+ * Akan diberikan sebuah angka N. Dan akan diberikan N angka.
+ */
+
+let input = [4, 21, 9, 11, 72.004]
+let output = Math.max(...input)
+console.log(output);
