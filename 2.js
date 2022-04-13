06a51b6d2c1e8a3211da252350d67814bb7ba3f5
Hifdzi Maulana <hifdzimaulana12@gmail.com>
2022-04-13 07:26:39 +0700

    soal nomor 2

---
+++ 2.js
@@ -0,0 +1,9 @@
+// Ganjil Atau Genap?
+
+/*
+ * Akan diberikan sebuah angka, silahkan tentukan apakah angka tersebut ganjil atau genap.
+ */
+
+const input = 7.001
+const output = input % 2 == 0 ? 'Genap' : 'Ganjil'
+console.log(output)
